d41e6ffd892fe2318d89e55a3295c8e0840e6650
Yash Chaubey <yashchaubey217@gmail.com>
2024-11-16 02:56:03 +0530

    nikita

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Yash Chaubey",
-  profile: '/myprofile.jpeg',
+  profile: '/myprofile.png',
   designation: "Computer Science Engineer and Aspiring Developer",
   description: "As a dedicated front-end developer, my passion lies in harnessing technology to create seamless digital experiences. With a solid programming foundation and a knack for solving complex challenges, I specialize in crafting robust software applications that not only meet but exceed user expectations. My approach is rooted in understanding user needs and translating them into intuitive interfaces. I thrive on the creativity of front-end development, constantly exploring new technologies and methodologies to deliver innovative solutions. Whether it's optimizing performance, enhancing usability, or ensuring compatibility across devices, I'm committed to pushing the boundaries of what's possible.",
   email: 'yash.mspn@gmail.com',
